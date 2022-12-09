6ff2a42d0c14e6bcee983134d47b5c3b31d5655a
git <nicolasbrun3094@hotmail.com>
2022-12-09 15:57:18 +0100

    Correction page produit validation panie

--- front/js/product.js
+++ front/js/product.js
@@ -1,23 +1,19 @@
 function redirectionIndex() {
   document.location.href = "./index.html";
-  swal({
-    title: "Votre article à bien était ajouté à votre panier",
-    text: "Vous allez être redirigé vers la page d'accueil",
-    icon: "success",
-    button: "Accueil",
-  });
 }
+
 const img = document.querySelector(".item__img");
 const price = document.getElementById("price");
 const desc = document.getElementById("description");
 const color = document.getElementById("colors");
 const button = document.getElementById("addToCart");
+
 let sofaData = [];
 let params = new URL(document.location).searchParams;
 let id = params.get("productid");
 
-console.log(sofaData);
-// Affichage API’
+// Affichage API //
+
 const fetchSofa = async () => {
   await fetch(`http://localhost:3000/api/products/${id}`)
     .then((res) => res.json())
@@ -26,9 +22,9 @@ const fetchSofa = async () => {
   console.log(sofaData);
 };
 
-// Affichage du produit
+// Affichage du produit //
 
-const sofaDispaly = async () => {
+const sofaDisplay = async () => {
   await fetchSofa();
 
   price.innerHTML = `<span>${sofaData.price}</span>`;
@@ -51,9 +47,9 @@ const sofaDispaly = async () => {
 
     if (color === "" || color === null) {
       alert("Veuillez choisir une couleur");
-      return;
+      return false;
     }
-    redirectionIndex();
+    return true;
   }
 
   // Verification que la quantité ne soit pas inférieur à 1//
@@ -64,45 +60,44 @@ const sofaDispaly = async () => {
     if (value < 1 || value > 100) {
       alert("Veuillez sélectionner un minimum de 1 article, au maximum 100 ");
       document.querySelector("#quantity").value = 1;
-      return;
+      return false;
     }
+    return true;
   }
 
   button.addEventListener("click", (e) => {
-    // Envoi des donnés au local storage
-    localStorage.setItem(
-      // Recuperer l'option selectionner
+    if (checkColor() && checkQuantity()) {
+      let modal = document.querySelector("#modal");
+      let btn_open = document.querySelector(".btn_show");
+      let btn_close = document.querySelector("#btn_close");
 
-      sofaData.name + color.value,
-      JSON.stringify({
-        id: sofaData._id,
-        name: sofaData.name,
-        color: color.value,
-        nbArticle: quantity.value,
-      })
-    );
-    checkColor();
-    checkQuantity();
+      modal.show();
+      setTimeout(function () {
+        modal.close();
+      }, 10000);
+
+      btn_close.addEventListener("click", () => {
+        modal.close();
+        redirectionIndex();
+      });
+
+      // Envoi des donnés au local storage //
+      localStorage.setItem(
+        // Recuperer l'option selectionner //
+        sofaData.name + color.value,
+        JSON.stringify({
+          id: sofaData._id,
+          name: sofaData.name,
+          color: color.value,
+          nbArticle: quantity.value,
+        })
+      );
+    }
   });
 };
-sofaDispaly();
+sofaDisplay();
 
 // BONUS //
 /*
-
 AJOUT D'UNE PASTILLE ROUGE AU NIVEAU DU PANIER
-
-*/
-
-// PROBLEMES //
-/*
-
-VALIDATION DES DEUX CONDITIONS :
-checkColor();
-checkQuantity();
-POUR ENSUITE ÊTRE REDIRIGÉ
-REDIRECTION VERS LA PAGE D'ACCUEIL (ALERT INSTANTANÉE)
-link tuto : "https://www.youtube.com/watch?v=43kJSL4sieE"
-site : "https://sweetalert2.github.io/#methods"
-
 */
