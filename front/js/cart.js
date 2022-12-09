e472531d6517726efc80b1ce43bcff8814116c1e
git <nicolasbrun3094@hotmail.com>
2022-12-09 05:56:06 +0100

    maj product + start cart

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,5 +1,95 @@
+// REGEX DES INPUTS
+/*
 const firstName = document.getElementById("firstName");
 const lastName = document.getElementById("lastName");
 const address = document.getElementById("address");
 const city = document.getElementById("city");
 const mail = document.getElementById("email");
+*/
+const inputs = document.querySelectorAll(
+  'input[type="text"], input[type="email"]'
+);
+
+// OPTI ?
+/*
+const errorDisplay =
+*/
+
+const firstNameChecker = (value) => {
+  const errorDisplay = document.getElementById("firstNameErrorMsg"); //DYNAMIQUE ?
+  if (value.length > 0 && (value.length < 3 || value.length > 20)) {
+    errorDisplay.textContent =
+      "Veuillez entrer un prénom entre 3 et 20 lettres";
+  } else if (!value.match(/^[a-zA-Z]*$/)) {
+    errorDisplay.textContent =
+      "Veuillez ne pas inclure de chiffres ou caractères spéciaux ";
+  } else {
+    errorDisplay.textContent = "";
+  }
+};
+
+const lastNameChecker = (value) => {
+  const errorDisplay = document.getElementById("lastNameErrorMsg"); //DYNAMIQUE ?
+  if (value.length > 0 && (value.length < 3 || value.length > 20)) {
+    errorDisplay.textContent =
+      "Veuillez entrer un nom de famille entre 3 et 20 lettres";
+  } else if (!value.match(/^[a-zA-Z]*$/)) {
+    errorDisplay.textContent =
+      "Veuillez ne pas inclure de chiffres ou caractères spéciaux ";
+  } else {
+    errorDisplay.textContent = "";
+  }
+};
+
+const addressChecker = (value) => {
+  const errorDisplay = document.getElementById("addressErrorMsg"); //DYNAMIQUE ?
+  if (!value.match(/^[a-zA-Z0-9 ]*$/)) {
+    errorDisplay.textContent = "Veuillez ne pas inclure caractères spéciaux ";
+  } else {
+    errorDisplay.textContent = "";
+  }
+};
+
+const cityChecker = (value) => {
+  const addressChecker = (value) => {
+    const errorDisplay = document.getElementById("cityErrorMsg"); //DYNAMIQUE ?
+    if (!value.match(/^[a-zA-Z0-9 ]*$/)) {
+      errorDisplay.textContent = "Veuillez ne pas inclure caractères spéciaux ";
+    } else {
+      errorDisplay.textContent = "";
+    }
+  };
+};
+
+const emailChecker = (value) => {
+  const errorDisplay = document.getElementById("emailErrorMsg"); //DYNAMIQUE ?
+  if (!value.match(/^[\w_-]+@[\w-]+\.[a-z]{2,4}$/i)) {
+    errorDisplay.textContent = "Veuillez saisir une adresse mail correcte";
+  } else {
+    errorDisplay.textContent = "";
+  }
+};
+
+inputs.forEach((input) => {
+  input.addEventListener("input", (e) => {
+    switch (e.target.id) {
+      case "firstName":
+        firstNameChecker(e.target.value);
+        break;
+      case "lastName":
+        lastNameChecker(e.target.value);
+        break;
+      case "address":
+        addressChecker(e.target.value);
+        break;
+      case "city":
+        cityChecker(e.target.value);
+        break;
+      case "email":
+        emailChecker(e.target.value);
+        break;
+      default:
+        null;
+    }
+  });
+});
--- front/js/product.js
+++ front/js/product.js
@@ -1,9 +1,11 @@
 function redirectionIndex() {
   document.location.href = "./index.html";
-  swal(
-    "Votre article à bien était ajouté à votre panier",
-    "Vous allez être redirigé vers la page d'accueil"
-  );
+  swal({
+    title: "Votre article à bien était ajouté à votre panier",
+    text: "Vous allez être redirigé vers la page d'accueil",
+    icon: "success",
+    button: "Accueil",
+  });
 }
 const img = document.querySelector(".item__img");
 const price = document.getElementById("price");
@@ -14,6 +16,7 @@ let sofaData = [];
 let params = new URL(document.location).searchParams;
 let id = params.get("productid");
 
+console.log(sofaData);
 // Affichage API’
 const fetchSofa = async () => {
   await fetch(`http://localhost:3000/api/products/${id}`)
@@ -37,14 +40,10 @@ const sofaDispaly = async () => {
                     </p>
                     `;
 
-  // OPTIMISER ?’”
-  /*
-  LOOP
-  */
-  color.innerHTML += `<option value="Vert">Vert</option>`;
-  color.innerHTML += `<option value="Rouge">Rouge</option>`;
-  color.innerHTML += `<option value="Bleu">Bleu</option>`;
-  button.innerHTML += `<a href="./index.html"></a>`;
+  let selectColor = `<option value="">--SVP, choissez une couleur --</option>`;
+  sofaData.colors.forEach((c) => {
+    selectColor += color.innerHTML += `<option value="${c}">${c}</option>`;
+  });
 
   // Verification qu'une couleur à bien était séléctioner //
   function checkColor() {
@@ -88,13 +87,22 @@ const sofaDispaly = async () => {
 };
 sofaDispaly();
 
-//////////////////////////////////////
-/*”
-Bonus :
-Ajout nb article
+// BONUS //
+/*
+
+AJOUT D'UNE PASTILLE ROUGE AU NIVEAU DU PANIER
+
 */
 
-// Ajout Local Storage ( Renvoi sur la page panier )
-// Pastille rouge du nombre article panier visuel ?
+// PROBLEMES //
+/*
 
-// Ajout Panier
+VALIDATION DES DEUX CONDITIONS :
+checkColor();
+checkQuantity();
+POUR ENSUITE ÊTRE REDIRIGÉ
+REDIRECTION VERS LA PAGE D'ACCUEIL (ALERT INSTANTANÉE)
+link tuto : "https://www.youtube.com/watch?v=43kJSL4sieE"
+site : "https://sweetalert2.github.io/#methods"
+
+*/
